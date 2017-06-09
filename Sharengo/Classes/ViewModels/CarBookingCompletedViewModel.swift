3117baa42a907035682d60442a8d623cc495ed89
Fabrizio Infante <f.infante@me.com>
2017-06-09 15:40:59 +0200

    WIP on Notifications inside app and Booking Completed VC

---
+++ Sharengo/Classes/ViewModels/CarBookingCompletedViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  CarBookingCompletedViewModel.swift
+//  Sharengo
+//
+//  Created by Dedecube on 09/06/17.
+//  Copyright © 2017 Dedecube. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import Boomerang
+
+final class CarBookingCompletedViewModel: ViewModelType {
+
+}
