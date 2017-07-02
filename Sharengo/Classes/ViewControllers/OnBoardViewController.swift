00d5cc69853b9938a44ace9b01bd9427063cb290
Fabrizio Infante <f.infante@me.com>
2017-07-02 09:49:21 +0200

    Added interface in storyboard and IBOutlet

--- Sharengo/Classes/ViewControllers/OnBoardViewController.swift
+++ Sharengo/Classes/ViewControllers/OnBoardViewController.swift
@@ -11,20 +11,19 @@ import RxSwift
 import RxCocoa
 import Boomerang
 import Gifu
-import DeviceKit
 
 class OnBoardViewController : UIViewController, ViewModelBindable {
-    @IBOutlet fileprivate weak var img_intro: GIFImageView!
-    @IBOutlet fileprivate weak var lbl_title1: UILabel!
-    @IBOutlet fileprivate weak var lbl_title2: UILabel!
-    @IBOutlet fileprivate weak var lbl_title3: UILabel!
+    @IBOutlet fileprivate weak var img_background: GIFImageView!
+    @IBOutlet fileprivate weak var lbl_description: UILabel!
+    @IBOutlet fileprivate weak var btn_skip: UIButton!
+    @IBOutlet fileprivate weak var pgc_steps: UIPageControl!
     
-    var viewModel: IntroViewModel?
+    var viewModel: OnBoardViewModel?
     
     // MARK: - ViewModel methods
     
     func bind(to viewModel: ViewModelType?) {
-        guard let viewModel = viewModel as? IntroViewModel else {
+        guard let viewModel = viewModel as? OnBoardViewModel else {
             return
         }
         self.viewModel = viewModel
@@ -35,19 +34,7 @@ class OnBoardViewController : UIViewController, ViewModelBindable {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.layoutIfNeeded()
-        self.view.constraint(withIdentifier: "bottomLblTitle1", searchInSubviews: true)?.constant = -110
-        self.view.constraint(withIdentifier: "bottomLblTitle2", searchInSubviews: true)?.constant = -160
-        self.view.constraint(withIdentifier: "bottomLblTitle3", searchInSubviews: true)?.constant = -150
-        self.lbl_title1.styledText = "lbl_introTitle1".localized()
-        self.lbl_title1.alpha = 0.0
-        self.lbl_title2.styledText = "lbl_introTitle2".localized()
-        self.lbl_title2.alpha = 0.0
-        self.lbl_title3.styledText = "lbl_introTitle3".localized()
-        self.lbl_title3.alpha = 0.0
-        if UserDefaults.standard.bool(forKey: "LongIntro") == false {
-            self.executeLongIntro()
-        } else {
-            self.executeShortIntro()
-        }
-    }    
+        self.lbl_description.styledText = "lbl_introTitle1".localized()
+        self.lbl_description.alpha = 0.0
+    }
 }
