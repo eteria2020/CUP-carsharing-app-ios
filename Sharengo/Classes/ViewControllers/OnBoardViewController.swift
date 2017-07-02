4d94f9f344e1c2db343edb7dcd3a5902460553c5
Fabrizio Infante <f.infante@me.com>
2017-07-02 09:59:16 +0200

    Added navigationbar

--- Sharengo/Classes/ViewControllers/OnBoardViewController.swift
+++ Sharengo/Classes/ViewControllers/OnBoardViewController.swift
@@ -11,8 +11,10 @@ import RxSwift
 import RxCocoa
 import Boomerang
 import Gifu
+import SideMenu
 
 class OnBoardViewController : UIViewController, ViewModelBindable {
+    @IBOutlet fileprivate weak var view_navigationBar: NavigationBarView!
     @IBOutlet fileprivate weak var img_background: GIFImageView!
     @IBOutlet fileprivate weak var lbl_description: UILabel!
     @IBOutlet fileprivate weak var btn_skip: UIButton!
@@ -34,9 +36,26 @@ class OnBoardViewController : UIViewController, ViewModelBindable {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.layoutIfNeeded()
+        
+        // NavigationBar
+        self.view_navigationBar.bind(to: ViewModelFactory.navigationBar(leftItemType: .home, rightItemType: .menu))
+        self.view_navigationBar.viewModel?.selection.elements.subscribe(onNext:{[weak self] output in
+            if (self == nil) { return }
+            switch output {
+            case .home:
+                Router.exit(self!)
+            case .menu:
+                self?.present(SideMenuManager.menuRightNavigationController!, animated: true, completion: nil)
+            default:
+                break
+            }
+        }).addDisposableTo(self.disposeBag)
+
+        // Labels
         self.lbl_description.styledText = "lbl_introTitle1".localized()
         self.lbl_description.alpha = 0.0
         
+        // Images
         self.img_background.animate(withGIFNamed: "ONBOARD_sfondo_loop.gif", loopCount: 0)
     }
 }
