f49df8540572a0dd4faa2dd9d766d75ff678740f
Fabrizio Infante <f.infante@me.com>
2017-07-12 11:12:04 +0200

    Updated attributes of model

--- Sharengo/Classes/Model/Feed.swift
+++ Sharengo/Classes/Model/Feed.swift
@@ -11,4 +11,12 @@ import RxSwift
 import Gloss
 
 public class Feed: ModelType {
+    var identifier: String = ""
+    var title: String = ""
+    var subtitle: String = ""
+    var description: String = ""
+    var icon: String = ""
+    var claim: String = ""
+    var date: String = ""
+    var advantage: String = ""
 }
