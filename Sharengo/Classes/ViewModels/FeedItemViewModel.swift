5e0806a62f0fdc1aa43b7c56683e6217a9f179ba
Fabrizio Infante <f.infante@me.com>
2017-07-12 11:14:04 +0200

    Updated itemviewmodel

--- Sharengo/Classes/ViewModels/FeedItemViewModel.swift
+++ Sharengo/Classes/ViewModels/FeedItemViewModel.swift
@@ -13,8 +13,22 @@ import Boomerang
 final class FeedItemViewModel : ItemViewModelType {
     var model:ItemViewModelType.Model
     var itemIdentifier:ListIdentifier = CollectionViewCell.feed
+    var title: String?
+    var subtitle: String?
+    var description: String?
+    var claim: String?
+    var date: String?
+    var advantage: String?
+    var icon: UIImage?
     
     init(model: Feed) {
         self.model = model
+        self.title = model.title
+        self.subtitle = model.subtitle
+        self.description = model.description
+        self.claim = model.claim
+        self.date = model.date
+        self.advantage = model.advantage
+        self.icon = UIImage(named: model.icon)
     }
 }
