779a247285ccf178d993b3e8a923bd66f7527dd5
Fabrizio Infante <f.infante@me.com>
2017-07-20 21:39:01 +0200

    wip on circular menu

--- Sharengo/Classes/Views/CircularMenuButton.swift
+++ Sharengo/Classes/Views/CircularMenuButton.swift
@@ -13,19 +13,43 @@ import Action
 
 class CircularMenuButton: UIButton {
     var circularMenuView: CircularMenuView?
+    var originalPoint: CGPoint?
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         self.circularMenuView?.touchesBegan(touches, with: event)
+        
+        self.originalPoint = self.superview?.convert(CGPoint.zero, from: self)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesMoved(touches, with: event)
         self.circularMenuView?.touchesMoved(touches, with: event)
+
+        if let point: CGPoint = touches.first?.location(in: self) {
+            if !((point.x < 0 || point.x > bounds.width) ||
+                (point.y < 0 || point.y > bounds.height)) {
+                if !(self.originalPoint != self.superview?.convert(CGPoint.zero, from: self))
+                {
+                    super.touchesMoved(touches, with: event)
+                }
+            }
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesEnded(touches, with: event)
         self.circularMenuView?.touchesEnded(touches, with: event)
+        
+        if let point: CGPoint = touches.first?.location(in: self) {
+            if !((point.x < 0 || point.x > bounds.width) ||
+                (point.y < 0 || point.y > bounds.height)) {
+                if !(self.originalPoint != self.superview?.convert(CGPoint.zero, from: self))
+                {
+                    super.touchesEnded(touches, with: event)
+                }
+            }
+        }
+        
+        self.isHighlighted = false
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
