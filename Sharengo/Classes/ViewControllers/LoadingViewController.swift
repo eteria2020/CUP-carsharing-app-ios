7637c522cea40bc5793014c77bc1139ab335286a
Elena Porcelli <elena@dedecube.com>
2017-08-18 22:25:50 +0200

    WIP: loader

--- Sharengo/Classes/ViewControllers/LoadingViewController.swift
+++ Sharengo/Classes/ViewControllers/LoadingViewController.swift
@@ -11,28 +11,12 @@ import SpinKit
 import SnapKit
 
 class LoadingViewController: UIViewController {
-    @IBOutlet fileprivate weak var blackView: UIView!
-    @IBOutlet fileprivate weak var containerView: UIView!
     
 	override func viewDidLoad() {
 		super.viewDidLoad()
-        self.setupInterface()
-	    UIView.animate(withDuration: 0.3, animations: { 
-            self.blackView.alpha = 1.0
-        }) 
     }
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
-	}
-	
-	func setupInterface() {
-        blackView.alpha = 0.0
-        let spinner: RTSpinKitView = RTSpinKitView(style: RTSpinKitViewStyle.stylePulse, color: UIColor.white)
-        containerView.backgroundColor = UIColor.clear
-        containerView.addSubview(spinner)
-        spinner.snp.makeConstraints { (make) -> Void in
-            make.center.equalTo(containerView)
-        }
 	}
 }
